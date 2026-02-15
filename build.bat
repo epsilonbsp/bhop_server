fd2e13327ad78f499077f1b0e4d231d26bfa94ed
epsilonbsp <deltasampler@gmail.com>
2026-02-15 21:51:24 +0200

    create server config

--- build.bat
+++ build.bat
@@ -72,5 +72,5 @@ if "%~1" == "build" (
 if "%~1" == "start" (
     cd build\game
 
-    .\srcds.exe -game cstrike -insecure +sv_cheats 1 +sv_airaccelerate 1000 +sv_enablebunnyhopping 1 -tickrate 100 +sv_maxcmdrate 100 +sv_maxupdaterate 100 +mp_autokick 0 +mp_freezetime 0 +bot_quota_mode normal +sv_hudhint_sound 0 +mp_ignore_round_win_conditions 1 +mp_autoteambalance 0 +mp_limitteams 0 +bot_join_after_player 0 +bot_dont_shoot 1 +bot_chatter off +mp_roundtime 0 +mp_autoteambalance 0 +sv_accelerate 5
+    .\srcds.exe -game cstrike -insecure -log -tickrate 100
 )
