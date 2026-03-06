363241796285e21f6ea0973aad06bfdff7688152
epsilonbsp <deltasampler@gmail.com>
2026-03-06 18:11:34 +0200

    add feet plugin to config

--- source/config.py
+++ source/config.py
@@ -86,6 +86,7 @@ class Resource_Key(IntEnum):
     RIPEXT = auto()
     DYNAMIC_CHANNELS = auto()
     BHOPTIMER = auto()
+    FEET = auto()
     GAP = auto()
     HEAD_BUG_FIX = auto()
     JUMPSTATS = auto()
@@ -354,6 +355,21 @@ RESOURCES[Resource_Key.BHOPTIMER] = Resource(
         os.path.join(REL_SM_SCRIPTING_DIR_PATH, "shavit-wr.sp"),
         os.path.join(REL_SM_SCRIPTING_DIR_PATH, "shavit-zones.sp"),
         os.path.join(REL_SM_SCRIPTING_DIR_PATH, "shavit-zones-json.sp")
+    ],
+    include_paths = [],
+    download_info = None,
+    unpack_info = None
+)
+
+RESOURCES[Resource_Key.FEET] = Resource(
+    type = Resource_Type.PLUGIN,
+    key = "feet",
+    name = "Feet",
+    enabled = True,
+    install_dir = os.path.join(PLUGINS_DIR_PATH, "feet"),
+    merge_paths = [],
+    plugin_paths = [
+        os.path.join(REL_SM_SCRIPTING_DIR_PATH, "feet.sp")
     ],
     include_paths = [],
     download_info = None,
